739a1d0d83184c93e410fb8c8232c183477597a0
zhangzuqiao <zhangzuqiao@bytedance.com>
2022-09-15 20:54:32 +0800

    只拉取最新数据

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import akshare as ak
 import datetime
 from dateutil.relativedelta import relativedelta
 import sqlite3
-
-conn = sqlite3.connect('data.sqlite')
+import time
+conn = sqlite3.connect('../data.sqlite')
 
 #stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
 #stock_zh_a_spot_em_df.to_sql('code', con=conn, if_exists='append', index=True)
@@ -13,6 +13,15 @@ cursor=conn.cursor()
 end = datetime.datetime.now()
 start = end - relativedelta(months=3)
 #start = end - relativedelta(days=10)
+
+cursor.execute("SELECT \"日期\" FROM stock ORDER BY \"日期\" DESC LIMIT 1")
+
+lastDate = ""
+for row in cursor:
+	lastDate = row[0]
+	print("lastDate: %s", lastDate)
+
+lastDate_timeStamp = int(time.mktime(time.strptime(lastDate, '%Y-%m-%d')))
 
 exit = False
 try:
@@ -48,23 +57,28 @@ for row in cursor:
 for code_ in code_list:
 	index +=1
 	print(code_)
-	stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=code_, period="daily", start_date=start.strftime("%Y%m%d"),
+	stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=code_, period="daily", start_date=lastDate,
 											end_date=end.strftime("%Y%m%d"), adjust="")
 	for value in stock_zh_a_hist_df.values:
-		sql = "insert into stock(股票代码,日期,开盘,收盘,最高,最低,成交量,成交额,振幅,涨跌幅,涨跌额,换手率) values ('"
-		sql += code_ + "','"
-		sql += str(value[0]) + "',"
-		sql += str(value[1]) + ","
-		sql += str(value[2]) + ","
-		sql += str(value[3]) + ","
-		sql += str(value[4]) + ","
-		sql += str(value[5]) + ","
-		sql += str(value[6]) + ","
-		sql += str(value[7]) + ","
-		sql += str(value[8]) + ","
-		sql += str(value[9]) + ","
-		sql += str(value[10]) + ")"
-		print(sql)
-		cursor.execute(sql)
+		date = value[0]
+		date_timeStamp = int(time.mktime(time.strptime(date, '%Y-%m-%d')))
+		# print(date_timeStamp)
+		if date_timeStamp > lastDate_timeStamp:
+			print(value)
+			sql = "insert into stock(股票代码,日期,开盘,收盘,最高,最低,成交量,成交额,振幅,涨跌幅,涨跌额,换手率) values ('"
+			sql += code_ + "','"
+			sql += str(value[0]) + "',"
+			sql += str(value[1]) + ","
+			sql += str(value[2]) + ","
+			sql += str(value[3]) + ","
+			sql += str(value[4]) + ","
+			sql += str(value[5]) + ","
+			sql += str(value[6]) + ","
+			sql += str(value[7]) + ","
+			sql += str(value[8]) + ","
+			sql += str(value[9]) + ","
+			sql += str(value[10]) + ")"
+			print(sql)
+			cursor.execute(sql)
 	conn.commit()
 conn.close()
